054359b6861bd0b22d465c1a4973ba66ddea9dd3
oswaldoj <oswjimenez@courseraAngularJS.com>
2016-10-01 12:12:20 -0400

    Final solution for assignment 1.

--- module1-solution/README.txt
+++ module1-solution/README.txt
@@ -2,4 +2,4 @@ Solution for Assignment 1 from the course AngularJS at Coursera, Prof. Yaakov
 
 Student:  Oswaldo José Jiménez Paredes
 
-GitHub repository 
+GitHub repository:    https://oswaldoj.github.io/AngularJS-assignments/
--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,26 +7,46 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController ($scope) {
+  // Initialize scope and default variables
   $scope.dishes = "";
+  var mensajeFaltanDatos = "Please enter data first";
+  var mensajeDemasiado = "Too much!";
+  var mensajeExito = "Enjoy!";
+  var colorError = "red";
+  var colorExito = "green";
 
-  $scope.howMuchFood = function () {
+  $scope.cuantaComida = function () {
     if ($scope.dishes.length == 0) {
-      $scope.mensajeFinal = "Please enter data first";
+      actualizarMensaje(mensajeFaltanDatos,colorError);
+      return;
+    }
+
+    // Split input string into items using a separator
+    var platos = $scope.dishes.split(",");
+    var numberOfDishes = 0; // counter for number of items
+
+    // Count non empty items
+    for (var i=0;i<platos.length;i++) {
+      if (platos[i].trim().length) numberOfDishes++;
+    }
+
+    // Put corresponding message
+    if (numberOfDishes == 0) {
+      actualizarMensaje(mensajeFaltanDatos,colorError);
+    } else if (numberOfDishes > 3) {
+      actualizarMensaje(mensajeDemasiado,colorError);
     } else {
-      var platos = $scope.dishes.split(",");
-      var numberOfDishes = 0;
-
-      for (var i=0;i<platos.length;i++) {
-        if (platos[i].trim().length) numberOfDishes++;
-      }
-
-      $scope.mensajeFinal = "Enjoy!";
-      if (numberOfDishes > 3) {
-        $scope.mensajeFinal = "Too much!";
-      }
+      actualizarMensaje(mensajeExito,colorExito);
     }
 
   };
+
+  // Message manage function
+  function actualizarMensaje(mensaje,color) {
+    $scope.mensajeFinal = mensaje;
+    $scope.colorBorde = color;
+    $scope.colorMensaje = color;
+  }
 }
 
 })();
